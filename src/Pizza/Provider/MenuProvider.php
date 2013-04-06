d72b62450d642d98b4c3e75996be62b1108e7bdf
Dominik Zogg <dominik.zogg@gmail.com>
2013-04-06 22:14:36 +0200

    add logout button only if its a loggedin user

--- src/Pizza/Provider/MenuProvider.php
+++ src/Pizza/Provider/MenuProvider.php
@@ -3,8 +3,10 @@
 namespace Pizza\Provider;
 
 use Knp\Menu\MenuFactory;
+use Pizza\Entity\User;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
+use Symfony\Component\Security\Core\SecurityContext;
 use Symfony\Component\Translation\Translator;
 
 class MenuProvider implements ServiceProviderInterface
@@ -15,7 +17,11 @@ class MenuProvider implements ServiceProviderInterface
             $menu = $this->getMenuFactory($app)->createItem('root');
             $menu->addChild($this->getTranslator($app)->trans('nav.order'), array('route' => 'order_list'));
             $menu->addChild($this->getTranslator($app)->trans('nav.user'), array('route' => 'user_list'));
-            $menu->addChild($this->getTranslator($app)->trans('nav.logout'), array('route' => 'logout'));
+
+            if($this->getUser($app)) {
+                $menu->addChild($this->getTranslator($app)->trans('nav.logout'), array('route' => 'logout'));
+            }
+
             return $menu;
         };
 
@@ -40,5 +46,27 @@ class MenuProvider implements ServiceProviderInterface
     protected function getTranslator(Application $app)
     {
         return $app['translator'];
+    }
+
+    /**
+     * @param Application $app
+     * @return SecurityContext
+     */
+    protected function getSecurity(Application $app)
+    {
+        return $app['security'];
+    }
+
+    /**
+     * @param Application $app
+     * @return User|null
+     */
+    protected function getUser(Application $app)
+    {
+        $token = $this->getSecurity($app)->getToken();
+        if(is_null($token)) {
+            return null;
+        }
+        return $token->getUser();
     }
 }
