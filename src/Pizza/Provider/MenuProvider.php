51ff2fcdd80130adddfbc3d309c7a374810206ea
Dominik Zogg <dominik.zogg@gmail.com>
2013-04-20 18:11:01 +0200

    no $this in closures (doesn't work in php5.3)

--- src/Pizza/Provider/MenuProvider.php
+++ src/Pizza/Provider/MenuProvider.php
@@ -2,27 +2,29 @@
 
 namespace Pizza\Provider;
 
-use Knp\Menu\MenuFactory;
 use Pizza\Entity\User;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\SecurityContext;
-use Symfony\Component\Translation\Translator;
 
 class MenuProvider implements ServiceProviderInterface
 {
     public function register(Application $app)
     {
         $app['main_menu'] = function($app) {
-            $menu = $this->getMenuFactory($app)->createItem('root');
+            $menu = $app['knp_menu.factory']->createItem('root');
 
-            if(!is_null($this->getUser($app))) {
-                $menu->addChild($this->getTranslator($app)->trans('nav.order'), array('route' => 'order_list'));
-                if($this->getSecurity($app)->isGranted('ROLE_ADMIN')) {
-                    $menu->addChild($this->getTranslator($app)->trans('nav.user'), array('route' => 'user_list'));
+            if(is_null($app['security']->getToken())) {
+                $user = null;
+            } else {
+                $user = $app['security']->getToken()->getUser();
+            }
+
+            if(!is_null($user)) {
+                $menu->addChild($app['translator']->trans('nav.order'), array('route' => 'order_list'));
+                if($app['security']->isGranted('ROLE_ADMIN')) {
+                    $menu->addChild($app['translator']->trans('nav.user'), array('route' => 'user_list'));
                 }
-                $menu->addChild($this->getTranslator($app)->trans('nav.logout'), array('route' => 'logout'));
+                $menu->addChild($app['translator']->trans('nav.logout'), array('route' => 'logout'));
             }
 
             return $menu;
@@ -32,53 +34,4 @@ class MenuProvider implements ServiceProviderInterface
     }
 
     public function boot(Application $app) {}
-
-    /**
-     * @param Application $app
-     * @return MenuFactory
-     */
-    protected function getMenuFactory(Application $app)
-    {
-        return $app['knp_menu.factory'];
-    }
-
-    /**
-     * @param Application $app
-     * @return Translator
-     */
-    protected function getTranslator(Application $app)
-    {
-        return $app['translator'];
-    }
-
-    /**
-     * @param Application $app
-     * @return SecurityContext
-     */
-    protected function getSecurity(Application $app)
-    {
-        return $app['security'];
-    }
-
-    /**
-     * @param Application $app
-     * @return null|TokenInterface
-     */
-    protected function getToken(Application $app)
-    {
-        return $this->getSecurity($app)->getToken();
-    }
-
-    /**
-     * @param Application $app
-     * @return User|null
-     */
-    protected function getUser(Application $app)
-    {
-        $token = $token = $this->getToken($app);
-        if(is_null($token)) {
-            return null;
-        }
-        return $token->getUser();
-    }
 }
